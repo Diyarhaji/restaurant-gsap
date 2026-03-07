64116ebde2f646e5ab433fac7f2465c0e72a0e9d
Diyar Gohdar Haji <142308018+Diyarhaji@users.noreply.github.com>
2026-03-08 01:25:40 +0300

    Fix: corrected errors and updated project files

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+    plugins: [
+        react(),
+        tailwindcss(),
+    ],
+    base: "/gsap-restaurant/", // This must be here, after the plugins array
 })
